094f79dde49dc3107c14fd403becc6e70470cf4a
glocal-frontend-leader <73451596+m1gue21@users.noreply.github.com>
2024-10-31 03:08:48 -0500

    first commi

---
+++ src/utils/data.ts
@@ -0,0 +1,146 @@
+import professionalUpdate from "../assets/images/professionalUpdate.jpg";
+import engagementGap from "../assets/images/engagement-gap.jpg";
+import resinTechnique from "../assets/images/resinTechnique.jpg";
+
+const services = [
+  {
+    title: "Técnicas de Precisión en Resina",
+    features: [
+      "Segmentación Avanzada de Casos",
+      "Análisis Detallado de Anatomía Dental",
+      "Creación de Restauraciones Personalizadas",
+    ],
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" stroke-width="1.5" fill="currentColor" class="w-6 h-6"><path d="M288 0C241.4 0 207.4 33.8 195.7 77.3c-5.8-1.3-11.7-2.5-17.7-2.5C116 74.8 74.8 116 74.8 166.4c0 29.6 12.7 62 32.3 85.8c20.4 24.6 52.1 44.4 90.3 57.2L255.6 492c12.7 12.7 33.3 12.7 45.6 0l57.2-182.8c38.2-12.8 69.9-32.6 90.3-57.2c19.6-23.8 32.3-56.2 32.3-85.8C501.2 116 460 74.8 403 74.8c-6 0-11.9 1.2-17.7 2.5C368.6 33.8 334.6 0 288 0zM144 136c17.7 0 32 14.3 32 32s-14.3 32-32 32s-32-14.3-32-32s14.3-32 32-32zM432 136c17.7 0 32 14.3 32 32s-14.3 32-32 32s-32-14.3-32-32s14.3-32 32-32z"/></svg>`,
+  },
+  {
+    title: "Contenido Visual Impactante",
+    features: [
+      "Historias de Casos Clínicos",
+      "Imágenes de Alta Calidad de Resultados",
+      "Videos Interactivos de Procedimientos",
+    ],
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" stroke-width="1.5" fill="currentColor" class="w-6 h-6"><path d="M256 0c123.7 0 224 100.3 224 224S379.7 448 256 448S32 347.7 32 224S132.3 0 256 0zm0 64c-13.3 0-24 10.7-24 24V136h-48c-13.3 0-24 10.7-24 24s10.7 24 24 24h72c13.3 0 24-10.7 24-24V88c0-13.3-10.7-24-24-24z"/></svg>`,
+  },
+  {
+    title: "Resultados Tangibles para Pacientes",
+    features: [
+      "Métricas de Satisfacción del Paciente",
+      "Seguimiento de Retención y Recomendación",
+      "Optimización de Conversiones en Consultas",
+    ],
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" stroke-width="1.5" fill="currentColor" class="w-6 h-6"><path d="M368 128c0-53 43-96 96-96H368c-53 0-96 43-96 96v320c0 53 43 96 96 96H368 480c53 0 96-43 96-96V128c0-53-43-96-96-96H368 480 368c0 0 0 0 0 0zM96 368c0-53 43-96 96-96H96C43 272 0 315 0 368v32c0 53 43 96 96 96h32c53 0 96-43 96-96v-32c0-53-43-96-96-96H96z"/></svg>`,
+  },
+  {
+    title: "Soluciones Personalizadas",
+    features: [
+      "Cursos de Resina Adaptados",
+      "Asesoría Individualizada en Técnicas",
+      "Enfoque en Necesidades Específicas",
+    ],
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" stroke-width="1.5" fill="currentColor" class="w-6 h-6"><path d="M288 0C241.4 0 207.4 33.8 195.7 77.3c-5.8-1.3-11.7-2.5-17.7-2.5C116 74.8 74.8 116 74.8 166.4c0 29.6 12.7 62 32.3 85.8c20.4 24.6 52.1 44.4 90.3 57.2L255.6 492c12.7 12.7 33.3 12.7 45.6 0l57.2-182.8c38.2-12.8 69.9-32.6 90.3-57.2c19.6-23.8 32.3-56.2 32.3-85.8C501.2 116 460 74.8 403 74.8c-6 0-11.9 1.2-17.7 2.5C368.6 33.8 334.6 0 288 0z"/></svg>`,
+  },
+  {
+    title: "Soporte Confiable",
+    features: [
+      "Atención Directa de Especialistas",
+      "Respuesta Rápida a Consultas",
+      "Disponibilidad en Horarios Flexibles",
+    ],
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" stroke-width="1.5" fill="currentColor" class="w-6 h-6"><path d="M224 0c88.4 0 160 71.6 160 160S312.4 320 224 320S64 248.4 64 160S135.6 0 224 0zm0 112a48 48 0 1 0 0 96a48 48 0 1 0 0-96zm0-48c-79.5 0-144 64.5-144 144c0 63.5 41.6 117.4 96 136.4v15.6c0 8.8-7.2 16-16 16H96c-35.3 0-64 28.7-64 64s28.7 64 64 64h352c35.3 0 64-28.7 64-64s-28.7-64-64-64H368c-8.8 0-16-7.2-16-16v-15.6c54.4-18.9 96-72.9 96-136.4c0-79.5-64.5-144-144-144z"/></svg>`,
+  },
+  {
+    title: "Innovación en Técnica Dental",
+    features: [
+      "Análisis de Nuevas Tendencias",
+      "Campañas y Cursos Innovadores",
+      "Adopción Temprana de Técnicas Modernas",
+    ],
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" stroke-width="1.5" fill="currentColor" class="w-6 h-6"><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm306.7 69.1L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>`,
+  },
+];
+
+const solutions = [
+  {
+    title: "Perfecciona tu Técnica",
+    painPoint: "Lograr acabados naturales con resina puede ser un desafío.",
+    agitatepainPoint:
+      "Es frustrante cuando el esfuerzo no se traduce en resultados óptimos.",
+    solution:
+      "Aprende técnicas avanzadas en nuestro curso para lograr resultados impecables.",
+    img: resinTechnique,
+  },
+  {
+    title: "Confianza en Restauraciones",
+    painPoint: "Las restauraciones complejas en resina pueden intimidar.",
+    agitatepainPoint:
+      "La falta de seguridad limita los tratamientos que puedes ofrecer.",
+    solution:
+      "Domina las restauraciones complejas y mejora la calidad de tus tratamientos.",
+    img: professionalUpdate,
+  },
+  {
+    title: "Mantente Actualizado",
+    painPoint:
+      "Las técnicas en odontología estética evolucionan constantemente.",
+    agitatepainPoint:
+      "No actualizarse implica perder oportunidades de crecimiento.",
+    solution: "Aprende las técnicas más recientes y destaca en tu práctica.",
+    img: resinTechnique,
+  },
+];
+
+const faqs = [
+  {
+    question: "¿Cómo sé si necesito un tratamiento de resina?",
+    answer:
+      "El tratamiento de resina es ideal para restaurar dientes dañados o mejorar la estética dental. Durante tu consulta, evaluaremos tu caso y te recomendaremos el tratamiento adecuado según tus necesidades.",
+  },
+  {
+    question: "¿Qué beneficios tiene la resina sobre otros materiales?",
+    answer:
+      "La resina ofrece una apariencia natural, es menos invasiva y se adhiere de manera efectiva al diente. Además, permite realizar ajustes precisos para una estética ideal, sin comprometer la estructura dental.",
+  },
+  {
+    question: "¿Cuánto tiempo dura el tratamiento de resina?",
+    answer:
+      "El tratamiento de resina suele durar entre 30 y 60 minutos por diente, dependiendo de la complejidad. Realizamos el proceso en una sola visita para mayor comodidad.",
+  },
+  {
+    question: "¿Cuáles son los cuidados después de un tratamiento de resina?",
+    answer:
+      "Es recomendable evitar alimentos duros o pegajosos y mantener una buena higiene dental. Además, es importante realizar visitas de control para asegurar la durabilidad del tratamiento.",
+  },
+  {
+    question: "¿La resina dental se mancha con el tiempo?",
+    answer:
+      "La resina puede llegar a mancharse con el consumo de alimentos como café o vino. Sin embargo, con un buen cuidado y visitas regulares al dentista, es posible mantener su aspecto óptimo por más tiempo.",
+  },
+  {
+    question: "¿El tratamiento de resina es doloroso?",
+    answer:
+      "No, el tratamiento de resina es generalmente indoloro. En casos de sensibilidad, utilizamos anestesia local para garantizar tu comodidad durante el procedimiento.",
+  },
+  {
+    question: "¿Qué tipo de anestesia se usa en los tratamientos de resina?",
+    answer:
+      "Utilizamos anestesia local cuando es necesario, lo que ayuda a minimizar cualquier molestia durante el procedimiento y asegurar tu comodidad en todo momento.",
+  },
+  {
+    question: "¿Cuánto tiempo dura una resina dental?",
+    answer:
+      "Con los cuidados adecuados y revisiones periódicas, las resinas dentales pueden durar entre 5 y 10 años. La durabilidad depende de factores como el cuidado oral y el tipo de alimentos que consumes.",
+  },
+  {
+    question: "¿Ofrecen financiamiento para los tratamientos odontológicos?",
+    answer:
+      "Sí, ofrecemos opciones de financiamiento para facilitar el acceso a nuestros tratamientos. Durante tu consulta, podremos discutir las alternativas que mejor se adapten a tus necesidades.",
+  },
+  {
+    question:
+      "¿Puedo agendar una consulta para evaluar mis necesidades antes de decidir un tratamiento?",
+    answer:
+      "¡Por supuesto! Te ofrecemos una consulta inicial para evaluar tu caso, responder a tus preguntas y definir el mejor tratamiento según tus objetivos y necesidades.",
+  },
+];
+
+export { services, solutions, faqs };
